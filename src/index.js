b4d55cff5af3f744f852dab750635d9f1efc1ab9
Capi Etheriel <barraponto@gmail.com>
2017-04-18 21:50:20 -0300

    fix(middleware): lints only files in the source
    Fixes #1

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 const merge = require('deepmerge');
 const StylelintPlugin = require('stylelint-webpack-plugin');
 
-const defaultOptions = {
-  // overrides Sass-only defaults (see JaKXz/stylelint-webpack-plugin#8)
-  files: ['**/*.+(css|scss|sass|less)'],
-  // temporary fix for HMR issues with stylelint errors
-  // (see JaKXz/stylelint-webpack-plugin#24)
-  failOnError: false
-};
 
-module.exports = ({ config }, options = {}) => {
-  config
+module.exports = (neutrino, options = {}) => {
+  const defaultOptions = {
+    // overrides Sass-only defaults (see JaKXz/stylelint-webpack-plugin#8)
+    files: ['**/*.+(css|scss|sass|less)'],
+    // only process source files (see barraponto/neutrino-preset-stylelint-standard/pull/1)
+    context: neutrino.options.source,
+    // temporary fix for HMR issues with stylelint errors
+    // (see JaKXz/stylelint-webpack-plugin#24)
+    failOnError: false
+  };
+
+  neutrino.config
     .plugin('stylelint')
     // let project options override default options.
     .use(StylelintPlugin, [merge(defaultOptions, options)]);
